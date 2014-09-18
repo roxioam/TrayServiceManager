8f27a533e246191b937d60c9210109db26cc536e
Alexander Melnikov <alexander_melnikov@epam.com>
2014-09-18 15:19:27 +0400

    Refactoring

--- TrayServiceManager/TrayServiceManagerContext.cs
+++ TrayServiceManager/TrayServiceManagerContext.cs
@@ -27,14 +27,10 @@ namespace TrayServiceManager
         {
             _serviceController = serviceController;
 
-            _notifyIcon = new NotifyIcon
-            {
-                Text = GetNotifyIconText(serviceController),
-                Icon = Properties.Resources.ServicesIcon,
-                ContextMenuStrip = GenerateContextMenu()
-            };
+            _notifyIcon = new NotifyIcon { Icon = Properties.Resources.ServicesIcon };
+            _notifyIcon.DoubleClick += NotifyIcon_DoubleClick;
 
-            _notifyIcon.DoubleClick += _notifyIcon_DoubleClick;
+            UpdateNotifyIcon();
 
             Application.ApplicationExit += (sender, e) => { _notifyIcon.Visible = false; };
 
@@ -45,88 +41,65 @@ namespace TrayServiceManager
 
         #region Private Methods
 
-        private string GetNotifyIconText(ServiceController serviceController)
+        private void UpdateNotifyIcon()
         {
-            return string.Format("{0} ({1})", serviceController.DisplayName, serviceController.Status);
+            UpdateNotifyIconText();
+            UpdateNotifyIconMenu();
         }
 
-        private ContextMenuStrip GenerateContextMenu()
+        private void UpdateNotifyIconText()
         {
-            var contextMenu = new ContextMenuStrip { Name = "TrayIconContextMenu", Size = new Size(153, 70) };
-
-            contextMenu.SuspendLayout();
-
-            var closeMenuItem = new ToolStripMenuItem
-            {
-                Name = "CloseMenuItem",
-                Size = new Size(152, 22),
-                Text = "Exit"
-            };
-
-            closeMenuItem.Click += (sender, e) => Application.Exit();
-
-            contextMenu.Items.AddRange(new ToolStripItem[] { closeMenuItem });
-
-            contextMenu.ResumeLayout();
-
-            return contextMenu;
+            _notifyIcon.Text = string.Format("{0} ({1})", _serviceController.DisplayName, _serviceController.Status);
         }
 
-        private void _notifyIcon_DoubleClick(object sender, System.EventArgs e)
+        private void UpdateNotifyIconMenu()
         {
-            switch (_serviceController.Status)
+            if (_notifyIcon.ContextMenuStrip == null)
+                _notifyIcon.ContextMenuStrip = new ContextMenuStrip { Name = "TrayIconContextMenu", Size = new Size(153, 70) };
+
+            _notifyIcon.ContextMenuStrip.SuspendLayout();
+
+            var closeMenuItem = _notifyIcon.ContextMenuStrip.Items["CloseMenuItem"] as ToolStripMenuItem;
+            if (closeMenuItem == null)
             {
-                case ServiceControllerStatus.Stopped:
-                    {
-                        _serviceController.Start();
-                        _serviceController.WaitForStatus(ServiceControllerStatus.Running, TimeSpan.FromMinutes(1));
-                        break;
-                    }
-                case ServiceControllerStatus.StartPending:
-                    {
-                        _serviceController.Refresh();
-                        _serviceController.WaitForStatus(ServiceControllerStatus.Running, TimeSpan.FromMinutes(1));
-                        break;
-                    }
-                case ServiceControllerStatus.StopPending:
-                    {
-                        _serviceController.Refresh();
-                        _serviceController.WaitForStatus(ServiceControllerStatus.Stopped, TimeSpan.FromMinutes(1));
-                        break;
-                    }
-                case ServiceControllerStatus.Running:
-                    {
-                        if (_serviceController.CanStop)
-                        {
-                            _serviceController.Stop();
-                            _serviceController.WaitForStatus(ServiceControllerStatus.Stopped, TimeSpan.FromMinutes(1));
-                        }
-                        break;
-                    }
-                case ServiceControllerStatus.ContinuePending:
-                    {
-                        _serviceController.Refresh();
-                        _serviceController.WaitForStatus(ServiceControllerStatus.Running, TimeSpan.FromMinutes(1));
-                        break;
-                    }
-                case ServiceControllerStatus.PausePending:
-                    {
-                        _serviceController.Refresh();
-                        _serviceController.WaitForStatus(ServiceControllerStatus.Paused, TimeSpan.FromMinutes(1));
-                        break;
-                    }
-                case ServiceControllerStatus.Paused:
-                    {
-                        if (_serviceController.CanPauseAndContinue)
-                        {
-                            _serviceController.Continue();
-                            _serviceController.WaitForStatus(ServiceControllerStatus.Running, TimeSpan.FromMinutes(1));
-                        }
-                        break;
-                    }
+                closeMenuItem = new ToolStripMenuItem
+                {
+                    Name = "CloseMenuItem",
+                    Size = new Size(152, 22),
+                    Text = "Exit"
+                };
+
+                closeMenuItem.Click += (sender, e) => Application.Exit();
+
+                _notifyIcon.ContextMenuStrip.Items.Add(closeMenuItem);
             }
 
-            _notifyIcon.Text = GetNotifyIconText(_serviceController);
+            _notifyIcon.ContextMenuStrip.ResumeLayout();
+        }
+
+        private void NotifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (_serviceController.Status == ServiceControllerStatus.Stopped || _serviceController.Status == ServiceControllerStatus.StopPending)
+                {
+                    _serviceController.Start();
+                    _serviceController.WaitForStatus(ServiceControllerStatus.Running, TimeSpan.FromMinutes(1));
+                }
+                else
+                {
+                    _serviceController.Stop();
+                    _serviceController.WaitForStatus(ServiceControllerStatus.Stopped, TimeSpan.FromMinutes(1));
+                }
+            }
+            catch (Exception ex)
+            {
+                _notifyIcon.ShowBalloonTip(1000, "Error", ex.Message, ToolTipIcon.Error);
+                throw;
+            }
+
+            _serviceController.Refresh();
+            UpdateNotifyIcon();
         }
 
         #endregion
